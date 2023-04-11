2bd974a4e2187e210c46cd6bb740cbc1c5967feb
Wilian-N-Silva <wilian.silva209@gmail.com>
2023-04-11 12:44:41 -0300

    refactor: :recycle: Refactor home structure and button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
 export function Home() {
   return (
-    <>
-      <h1>Caption Generator</h1>
-      <h2>Legende seus vídeos de maneira rápida e exporte como CRT</h2>
-    </>
+    <div className="home">
+      <header className="home__header">
+        <h1 className="home__title">Caption Generator</h1>
+        <h2 className="home__subtitle">
+          Legende seus vídeos de maneira rápida e exporte como CRT
+        </h2>
+      </header>
+
+      <form className="home__form">
+        <div className="text-field">
+          <label className="text-field__label" htmlFor="url-input">
+            URL do vídeo
+          </label>
+          <input className="text-field__input" id="url-input" type="url" />
+        </div>
+        <button className="button" type="submit">
+          Procurar
+        </button>
+      </form>
+    </div>
   )
 }
